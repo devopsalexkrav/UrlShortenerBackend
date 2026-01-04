c75b06e4e333a5096667ba3a55c4a819c2e7776c
Alexander <AlexanderEmail@test.com>
2026-01-05 00:06:01 +0300

    add project

---
+++ internal/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string `yaml:"address" env-default:"localhost:8082"`
+	Timeout     string `yaml:"timeout" env-default:"4s"`
+	IdleTimeout string `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("failed to parse config: %s", err)
+	}
+
+	return &cfg
+}
